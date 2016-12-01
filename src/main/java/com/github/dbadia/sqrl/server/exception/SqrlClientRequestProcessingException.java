2bea33c2c0f3f624afdf24dee47da77078cdb9f8
Dave Badia <dbadiaoss@gmail.com>
2016-12-01 03:35:16 -0500

    embed sqrl client details in client request processing exceptions

--- src/main/java/com/github/dbadia/sqrl/server/exception/SqrlClientRequestProcessingException.java
+++ src/main/java/com/github/dbadia/sqrl/server/exception/SqrlClientRequestProcessingException.java
@@ -1,5 +1,7 @@
 package com.github.dbadia.sqrl.server.exception;
 
+import com.github.dbadia.sqrl.server.backchannel.SqrlLoggingUtil;
+
 /**
  * Indicates that an error occurred (nut token timeout, invalid signature, etc) while processing a request from a SQRL
  * client, will trigger the setting of the provided tif on the response
@@ -11,7 +13,7 @@ public class SqrlClientRequestProcessingException extends SqrlException {
 	private final int			tifToAdd;
 
 	public SqrlClientRequestProcessingException(final int tifToAdd, final String message) {
-		super(message);
+		super(new StringBuilder(SqrlLoggingUtil.getLogHeader()).append(message).toString());
 		this.tifToAdd = tifToAdd;
 	}
 
